190aecea07e08ee0ffbba4bf4ad0d376c59b98c8
PavelJavaGitHub <105481448+PavelJavaGitHub@users.noreply.github.com>
2022-05-30 22:50:49 +0300

    task 2.1.2 completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHellWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+//        System.out.println(beanHellWorld1.getMessage());
+
+        HelloWorld beanHellWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(beanHellWorld1 == beanHellWorld2);
+
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1 == beanCat2);
     }
 }
